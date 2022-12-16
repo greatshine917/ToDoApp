ffdaa81d3c23db9578c20a6e77095d09570f7cad
greatshine917 <j01039519778@gmail.com>
2022-12-16 21:54:54 +0900

    update-dev

---
+++ src/main/java/com/codestates/TodoApplication/ToDoController.java
@@ -0,0 +1,12 @@
+package com.codestates.TodoApplication;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ToDoController {
+    @GetMapping("/")
+    public String toDo() {
+        return "To-Do Application !";
+    }
+}
